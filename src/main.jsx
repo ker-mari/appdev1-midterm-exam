4b73d525e87589f18331eb2a5566a9ebbeea3ec9
ker-mari <magmayoker@gmail.com>
2025-10-04 14:34:20 +0800

    error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 // import './index.css'
-=======
-import './index.css'
->>>>>>> 3027302e6d68324d60b2bd2726908b628ca5a7b1
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: "/appdev1-midterm-exam/",
   plugins: [react()],
 });
-=======
-  plugins: [react()],
-})
->>>>>>> 3027302e6d68324d60b2bd2726908b628ca5a7b1
